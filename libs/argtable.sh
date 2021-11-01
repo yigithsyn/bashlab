a6dbc0b8351df75f5f0335eaf10c528ba955693f
Hüseyin YİĞİT <iletisim@huseyinyigit.com.tr>
2021-11-01 09:08:02 +0300

    bootstrap file

--- libs/argtable.sh
+++
@@ -1,3 +0,0 @@
-wget -O libs/argtable3.tar.gz  https://github.com/argtable/argtable3/releases/download/v3.2.1.52f24e5/argtable-v3.2.1.52f24e5-amalgamation.tar.gz
-mkdir libs/argtable3
-tar -xvf libs/argtable3.tar.gz --directory libs/argtable3
