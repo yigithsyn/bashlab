8723733f0b7dbc3ec51297ab98c1c7ad8589ddc0
Hüseyin <iletisim@huseyinyigit.com.tr>
2021-11-02 22:36:51 +0300

    prettify

--- bootstrap.bat
+++ bootstrap.bat
@@ -16,25 +16,35 @@ SET filename=cmake.exe
 FOR /R C:\ %%a IN (\) DO (
    IF EXIST "%%a\%filename%" (
 
-      SET fullpath=%%a%filename%
+      SET cmake_path=%%a%filename%
       GOTO break
    )
 )
 :break
 
-ECHO %fullpath%
-"%fullpath%" -version
+@REM FOR /F %%i IN ('%cmake_path% --version') DO set cmake_vers=%%i
+@REM for /f "delims=$ tokens=1*" %%i in ('%cmake_path% --version 2^>^&1 1^>nul ') do echo %%i
+
+ECHO ============================
+ECHO Building ...
+ECHO ============================
+ECHO CMake path   : %cmake_path%
+ECHO CMake version: 
+'%cmake_path% --version'
+
 reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OS=32BIT || set OS=64BIT
 
 if %OS%==32BIT echo This is a 32bit operating system
 if %OS%==64BIT echo This is a 64bit operating system
 
-if %OS%==32BIT "%fullpath%" -DCMAKE_INSTALL_PREFIX=%USERPROFILE%\AppData\Local .. -T host=x86 -A Win32
-if %OS%==64BIT "%fullpath%" -DCMAKE_INSTALL_PREFIX=%USERPROFILE%\AppData\Local .. -T host=x64 -A x64
+if %OS%==32BIT "%cmake_path%" -DCMAKE_INSTALL_PREFIX=%USERPROFILE%\AppData\Local .. -T host=x86 -A Win32
+if %OS%==64BIT "%cmake_path%" -DCMAKE_INSTALL_PREFIX=%USERPROFILE%\AppData\Local .. -T host=x64 -A x64
 
-"%fullpath%" --build . --config Debug
+"%cmake_path%" --build . --config Debug
 
-ECHO "[INFO] Installing ..."
-"%fullpath%" --build . --target INSTALL --config MinSizeRel
+ECHO ============================
+ECHO Installing ...
+ECHO ============================
+"%cmake_path%" --build . --target INSTALL --config MinSizeRel
 
 CD ..
