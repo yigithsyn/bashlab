ed96bc74d59e2fd2c9dc8848b7ecb699e7db1642
Hüseyin YİĞİT <iletisim@huseyinyigit.com.tr>
2021-11-11 13:00:49 +0300

    organize for jansson library

--- bootstrap.bat
+++ bootstrap.bat
@@ -22,11 +22,12 @@ ECHO [INFO] Dependencies ...
 ECHO ============================
 RMDIR /Q /S libs 
 MKDIR libs
-ECHO "Downloading argtable3 ..."
+ECHO Argtable3: A single-file, ANSI C, command-line parsing library that parses GNU-style command-line options.
 curl -L https://github.com/argtable/argtable3/releases/download/v3.2.1.52f24e5/argtable-v3.2.1.52f24e5-amalgamation.tar.gz --output libs\argtable3.tar.gz --silent 
 MKDIR libs\argtable3
 tar -xf libs\argtable3.tar.gz --directory libs\argtable3
 
+ECHO Jansson: C library for encoding, decoding and manipulating JSON data
 curl -L https://github.com/akheron/jansson/releases/download/v2.14/jansson-2.14.tar.gz --output libs\jansson-2.14.tar.gz --silent
 tar -xvf libs\jansson-2.14.tar.gz --directory libs\
 CD libs\jansson-2.14
@@ -34,8 +35,8 @@ MKDIR build
 CD build
 "%cmake_path%" -DJANSSON_BUILD_SHARED_LIBS=1 -DJANSSON_BUILD_DOCS=OFF ..
 "%cmake_path%" --build . --config Release
-CD ..
-CD ..
+CD ../../..
+
 
 ECHO ============================
 ECHO [INFO] Building ...
