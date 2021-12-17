7dae43b51c411252ca4f49573124bf0dfaf3b577
Hüseyin YİĞİT <iletisim@huseyinyigit.com.tr>
2021-12-17 16:54:49 +0300

    some test

--- include/programs.h
+++ include/programs.h
@@ -10,7 +10,7 @@ static const char *programs =
     /*        */"{\"num\":\"1.0.0\", \"msg\":\"Initial release\"}"
     /*      */"],"
     "\"pargs\": ["
-    /*        */"{\"name\":\"s11\", \"minc\":1, \"maxc\":1, \"desc\":\"Reflection coefficient (S11) value(s)\"}"
+    /*        */"{\"name\":\"s11\", \"minc\":1, \"maxc\":100, \"desc\":\"Reflection coefficient (S11) value(s)\"}"
     /*       */"],"
     "\"oargs\": ["
     /*        */"{\"short\":\"d\", \"long\":\"db\", \"minc\":0, \"maxc\":1, \"desc\":\"Parse S11 value(s) in decibel [dB]\"}"
