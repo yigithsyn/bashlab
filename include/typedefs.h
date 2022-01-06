e2bd4b46321e0d4e98189363b95cd3d1ddef1886
Hüseyin YİĞİT <iletisim@huseyinyigit.com.tr>
2022-01-06 16:58:25 +0300

    string and number set

--- include/typedefs.h
+++ include/typedefs.h
@@ -4,8 +4,9 @@
 #include <stdint.h>
 
 typedef double number_t;
-typedef char *string_t;
-typedef const char* const_string_t;
+typedef char char_t;
+typedef char_t *string_t;
+typedef const char_t* const_string_t;
 typedef int32_t int_t;
 
 
