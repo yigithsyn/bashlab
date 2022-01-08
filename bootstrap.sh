e8ff5475a5c8edac8320764d75e9736a8a1b70b4
Hüseyin YİĞİT <iletisim@huseyinyigit.com.tr>
2022-01-09 00:06:10 +0300

    unix builds

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-if [ "$1" != "build" ]; then
-  ################################################################################
-  # dependencies
-  ################################################################################
-  echo ============================
-  echo "[INFO] Dependencies ..."
-  echo ============================
-  rm -rf libs && mkdir libs
+################################################################################
+# dependencies
+################################################################################
+echo ============================
+echo "[INFO] Dependencies ..."
+echo ============================
+rm -rf libs && mkdir libs
 
+echo "algo: Simple algorithms"
+git clone https://github.com/ntessore/algo.git libs/algo
+
+if [ "$1" == "argtable3" ]; then
   echo "Argtable3: A single-file, ANSI C, command-line parsing library that parses GNU-style command-line options"
   curl -L https://github.com/argtable/argtable3/releases/download/v3.2.1.52f24e5/argtable-v3.2.1.52f24e5.tar.gz --output libs/argtable-v3.2.1.52f24e5.tar.gz --silent 
   tar -xf libs/argtable-v3.2.1.52f24e5.tar.gz --directory libs/
@@ -16,11 +19,14 @@ if [ "$1" != "build" ]; then
   mkdir build
   cd build
   cmake -DBUILD_SHARED_LIBS=ON ..
-  cmake --build . --config Debug
   cmake --build . --config Release
   cmake --install . --prefix /usr/local
   cd ../../..
+  rm -rf libs/argtable-v3.2.1.52f24e5
+  exit
+fi
 
+if [ "$1" == "jansson" ]; then
   echo "Jansson: C library for encoding, decoding and manipulating JSON data"
   curl -L https://github.com/akheron/jansson/releases/download/v2.14/jansson-2.14.tar.gz --output libs/jansson-2.14.tar.gz --silent
   tar -xf libs/jansson-2.14.tar.gz --directory libs/
@@ -28,16 +34,16 @@ if [ "$1" != "build" ]; then
   mkdir build
   cd build
   cmake -DJANSSON_BUILD_DOCS=OFF -DJANSSON_BUILD_SHARED_LIBS=ON ..
-  cmake --build . --config Debug
   cmake --build . --config Release
   cmake --install . --prefix /usr/local
   cd ../../..
+  rm -rf libs/jansson-2.14
+  exit
+fi
 
-  echo "algo: Simple algorithms"
-  git clone https://github.com/ntessore/algo.git libs/algo
-
+if [ "$1" == "civetweb" ]; then
   echo ============================
-  echo "[INFO] Dependecies: Civetweb: BuEmbedded C/C++ web server"
+  echo "[INFO] Dependecies: Civetweb: Embedded C/C++ web server"
   echo ============================
   curl -L https://github.com/civetweb/civetweb/archive/refs/tags/v1.15.tar.gz --output libs/civetweb-1.15.tar.gz --silent
   tar -xvf libs/civetweb-1.15.tar.gz --directory libs/
@@ -45,11 +51,11 @@ if [ "$1" != "build" ]; then
   mkdir build_dir
   cd build_dir
   cmake -DCIVETWEB_ENABLE_SSL=OFF -DCIVETWEB_ENABLE_SERVER_EXECUTABLE=OFF -DCIVETWEB_ENABLE_DEBUG_TOOLS=OFF -DCIVETWEB_BUILD_TESTING=OFF -DBUILD_TESTING=OFF -DCIVETWEB_ENABLE_WEBSOCKETS=ON -DBUILD_SHARED_LIBS=ON ..
-  cmake --build . --config Debug
   cmake --build . --config Release
   cmake --install . --prefix /usr/local
   cd ../../..
-
+  rm -rf libs/civetweb-1.15
+  exit
 fi
 
 rm -rf build && mkdir build && cd build
