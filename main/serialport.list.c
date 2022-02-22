09ccacb9a5bfd48204942f48b11b0b97d41cd044
Hüseyin YİĞİT <iletisim@huseyinyigit.com.tr>
2022-02-22 12:02:01 +0300

    fix

--- main/serialport.list.c
+++ main/serialport.list.c
@@ -202,7 +202,7 @@ STDOUT:;
   size_t Nans = N1;
   char **ans = out1;
   for (int i = 0; i < Nans; i++)
-    fprintf(stdout, "%s: %s", sp_get_port_name(port_list[i]), sp_get_port_description(port_list[i]));
+    fprintf(stdout, "%s: %s\n", sp_get_port_name(port_list[i]), sp_get_port_description(port_list[i]));
   // if (Nans > 0)
   //   fprintf(stdout, "%s", ans[0]);
   // for (size_t i = 1; i < MIN(Nans, 3); ++i)
