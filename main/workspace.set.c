bff301825f465c2c921533b756acd313d6c073df
Hüseyin YİĞİT <iletisim@huseyinyigit.com.tr>
2022-04-08 09:26:00 +0300

    register program at the beginning

--- main/workspace.set.c
+++ main/workspace.set.c
@@ -71,6 +71,38 @@ int main(int argc, char *argv[])
   program = json_loads(program_json, 0, json_error);
 
   /* ======================================================================== */
+  /* register program                                                         */
+  /* ======================================================================== */
+  if (getenv("BASHLAB_MONGODB_URI_STRING"))
+  {
+    mongoc_init();
+    mdb_uri = mongoc_uri_new_with_error(getenv("BASHLAB_MONGODB_URI_STRING"), &mdb_err);
+    if (mdb_uri)
+    {
+      mdb_cli = mongoc_client_new_from_uri(mdb_uri);
+      if (mdb_cli)
+      {
+        mdb_col = mongoc_client_get_collection(mdb_cli, mdb_dtb_str, "programs");
+        sprintf(buff, "{\"$set\": %s }", program_json);
+        mdb_doc = bson_new_from_json((const uint8_t *)buff, -1, &mdb_err);
+        mdb_qry = BCON_NEW("name", BCON_UTF8(PROGNAME));
+        mdb_qry1 = BCON_NEW("upsert", BCON_BOOL(true));
+        if (!mongoc_collection_update_one(mdb_col, mdb_qry, mdb_doc, mdb_qry1, NULL, &mdb_err))
+        {
+          fprintf(stderr, "%s: program info update/insert failed: %s(%u)\n", PROGNAME, mdb_err.message, mdb_err.code);
+          exitcode = EXIT_FAILURE;
+          goto EXIT;
+        }
+        bson_destroy(mdb_doc);
+        bson_destroy(mdb_qry);
+        bson_destroy(mdb_qry1);
+
+        mongoc_collection_destroy(mdb_col);
+      }
+    }
+  }
+
+  /* ======================================================================== */
   /* argument parse                                                           */
   /* ======================================================================== */
   /* copy positional args */
@@ -171,27 +203,6 @@ int main(int argc, char *argv[])
   if (getenv("BASHLAB_MONGODB_COL_STRING"))
     strcpy(mdb_col_str, getenv("BASHLAB_MONGODB_COL_STRING"));
   mdb_col = mongoc_client_get_collection(mdb_cli, mdb_dtb_str, mdb_col_str);
-
-  /* ======================================================================== */
-  /* register command                                                         */
-  /* ======================================================================== */
-  mdb_col = mongoc_client_get_collection(mdb_cli, mdb_dtb_str, "commands");
-  sprintf(buff, "{\"$set\": %s }", program_json);
-  mdb_doc = bson_new_from_json((const uint8_t *)buff, -1, &mdb_err);
-  mdb_qry = BCON_NEW("name", BCON_UTF8(PROGNAME));
-  mdb_qry1 = BCON_NEW("upsert", BCON_BOOL(true));
-  if (!mongoc_collection_update_one(mdb_col, mdb_qry, mdb_doc, mdb_qry1, NULL, &mdb_err))
-  {
-    fprintf(stderr, "%s: command info update/insert failed: %s(%u)\n", PROGNAME, mdb_err.message, mdb_err.code);
-    exitcode = EXIT_FAILURE;
-    goto EXIT;
-  }
-  bson_destroy(mdb_doc);
-  bson_destroy(mdb_qry);
-  bson_destroy(mdb_qry1);
-
-  mongoc_collection_destroy(mdb_col);
-
 
 /* ======================================================================== */
 /* main operation                                                           */
