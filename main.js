6afcee94455e85574d4a6102d1821752053d2798
Hüseyin <iletisim@huseyinyigit.com.tr>
2022-06-16 23:01:21 +0300

    new model

---
+++ main.js
@@ -0,0 +1,8 @@
+
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+
+app.use('/api', createProxyMiddleware({ target: 'http://www.example.org', changeOrigin: true }));
+app.listen(3000);
